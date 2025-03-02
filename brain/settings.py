c5b62af9ef7d3a93785343b770ec26f1274cafd7
adamu <adams.square@yahoo.com>
2025-03-02 10:37:22 +0300

    update .conf in the media

--- brain/settings.py
+++ brain/settings.py
@@ -129,7 +129,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 import os
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "/opt/render/project/src/media/"
+MEDIA_ROOT = "/opt/render/project/src/"
 
 
 # Default primary key field type
