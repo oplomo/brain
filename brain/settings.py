3cd3799eb5a0f09c4741bbe512e86c3914c81311
adamu <adams.square@yahoo.com>
2025-03-07 12:45:26 +0300

    Jeruscore Version 1.0.0: Finally Here! This marks the official release of Jeruscore v1.0.0, bringing exciting features and improvements. Stay tuned for more updates!

--- brain/settings.py
+++ brain/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!kdbpe1)1l+3pqg@y!w)m^n#o32*j0#b(qhmg7izz-2%0iz6m^"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["www.jeruscore.com", "jeruscore.com", "brain-zofx.onrender.com"]
