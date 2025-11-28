24034ba7c1de8da688fe517f0f5d7fac9d7f0e87
plata oplomo <137695808+oplomo@users.noreply.github.com>
2025-11-28 14:29:25 +0300

    Configure DATABASES setting for SQLite
    Uncomment the DATABASES setting and update it to include SQLite configuration while commenting out the previous DATABASES definition.

--- brain/settings.py
+++ brain/settings.py
@@ -74,17 +74,17 @@ WSGI_APPLICATION = "brain.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 import os
 import dj_database_url
 
-DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+# DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
 
 
 AUTH_PASSWORD_VALIDATORS = [
